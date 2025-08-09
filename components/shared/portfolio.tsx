f562bf463d829f37ac688fcc3a755cc23faf96e8
Senator <senatorcox90@gmail.com>
2025-08-10 00:16:44 +0100

    Change position of toast notification

--- components/shared/portfolio.tsx
+++ components/shared/portfolio.tsx
@@ -10,9 +10,9 @@ const Portfolio = () => {
     <div className="portfolio grid lg:gap-24 lg:grid-cols-2 transition-all gap-10">
       <div className="w-full flex justify-center lg:justify-end">
         <ul
-          className={`${montserrat.className} flex flex-col  gap-4 md:items-end py-6 `}
+          className={`${montserrat.className} flex flex-col transition-all gap-4 md:items-end py-6 `}
         >
-          <li className="py-1  flex flex-row items-baseline gap-1 hover:-translate-y-1 hover:scale-110  duration-300">
+          <li className="py-1  flex flex-row transition-all items-baseline gap-1 hover:-translate-y-1 hover:scale-110  duration-300">
             <Link
               href="/webdev"
               className="text-white-400 text-3xl md:text-4xl text-white hover:text-lime-400"
@@ -21,7 +21,7 @@ const Portfolio = () => {
             </Link>
             <TfiAngleDoubleRight className=" md:text-2xl text-lime-500" />
           </li>
-          <li className="py-1  flex flex-row items-baseline gap-1 hover:-translate-y-1 hover:scale-110  duration-300">
+          <li className="py-1  flex flex-row transition-all items-baseline gap-1 hover:-translate-y-1 hover:scale-110  duration-300">
             <Link
               href="/webdev#follow-up"
               className="text-white-400 text-3xl md:text-4xl text-white hover:text-lime-400"
@@ -33,13 +33,13 @@ const Portfolio = () => {
           <li className="py-1  flex flex-row items-baseline gap-1 hover:-translate-y-1 hover:scale-110  duration-300">
             <Link
               href="/webdev#bespoke"
-              className="text-white-400 text-3xl md:text-4xl text-white hover:text-lime-400"
+              className="text-white-400 text-3xl transition-all md:text-4xl text-white hover:text-lime-400"
             >
               Bespoke Design
             </Link>
             <TfiAngleDoubleRight className="text-white-lime md:text-2xl text-lime-500" />
           </li>
-          <li className="py-1  flex flex-row items-baseline gap-1 hover:-translate-y-1 hover:scale-110  duration-300">
+          <li className="py-1  flex flex-row transition-all items-baseline gap-1 hover:-translate-y-1 hover:scale-110  duration-300">
             <Link
               href="/webdev#blogging"
               className="text-white-400 text-3xl  md:text-4xl text-white hover:text-lime-400"
@@ -48,7 +48,7 @@ const Portfolio = () => {
             </Link>
             <TfiAngleDoubleRight className="text-white-lime md:text-2xl text-lime-500" />
           </li>
-          <li className="py-1  flex flex-row items-baseline gap-1 hover:-translate-y-1 hover:scale-110  duration-300">
+          <li className="py-1  flex flex-row transition-all items-baseline gap-1 hover:-translate-y-1 hover:scale-110  duration-300">
             <Link
               href="webdev#ecommerce"
               className="text-white-400 text-3xl  md:text-4xl text-white hover:text-lime-400"
@@ -60,13 +60,13 @@ const Portfolio = () => {
           <li className="py-1  flex flex-row items-baseline gap-1 hover:-translate-y-1 hover:scale-110  duration-300">
             <Link
               href="webdev#seo"
-              className="text-white-400 text-3xl  md:text-4xl text-white hover:text-lime-400"
+              className="text-white-400 text-3xl transition-all md:text-4xl text-white hover:text-lime-400"
             >
               SEO/SMO
             </Link>
             <TfiAngleDoubleRight className="text-white-lime md:text-2xl text-lime-500" />
           </li>
-          <li className="py-1  flex flex-row items-baseline gap-1 hover:-translate-y-1 hover:scale-110  duration-300">
+          <li className="py-1  flex flex-row transition-all items-baseline gap-1 hover:-translate-y-1 hover:scale-110  duration-300">
             <Link
               href="webdev#cms"
               className="text-white-400 text-2xl md:text-4xl text-white hover:text-lime-400"
