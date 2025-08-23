30cda3890445a59428891bdeda1f15cf431328f2
Senator <senatorcox90@gmail.com>
2025-08-23 09:24:46 +0100

    Added slug generator to blog form

--- lib/actions/contactFormAction.ts
+++ lib/actions/contactFormAction.ts
@@ -24,7 +24,7 @@ export async function contactFormAction(
   const message = formData.get("message") as string;
 
   const { data, error } = await resend.emails.send({
-    from: "queries@londonvibes.net",
+    from: "onboardin@resend.dev",
     to: "senatorcox90@gmail.com",
     subject: `Client Request From LVL`,
     react: `${fullname}`,
