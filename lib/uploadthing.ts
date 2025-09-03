3e1ffac80300bac743bddd3d1ed1c7ac9de4c8a4
Senator <senatorcox90@gmail.com>
2025-09-03 23:48:40 +0100

    Resolved errors on create product page

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -2,7 +2,7 @@ import {
   generateUploadButton,
   generateUploadDropzone,
 } from "@uploadthing/react";
-import { UTApi } from "uploadthing/server";
+// import { UTApi } from "uploadthing/server";
 
 // export const utapi = new UTApi({
 //   // ...options,
