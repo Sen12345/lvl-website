3ef9a879e26b5b0221a9db6f59be9ffa39f700a0
Senator <senatorcox90@gmail.com>
2025-09-05 10:02:00 +0100

    Created a delete blog action

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,7 +1,6 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { auth } from "@/auth";
-import { NextRequest } from "next/server";
 
 const f = createUploadthing();
 
@@ -10,10 +9,6 @@ export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({
     image: {
-      /**
-       * For full list of options and defaults, see the File Route API reference
-       * @see https://docs.uploadthing.com/file-routes#route-config
-       */
       maxFileSize: "4MB",
       maxFileCount: 1,
     },
@@ -33,7 +28,7 @@ export const ourFileRouter = {
       // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
 
-      console.log("file url", file.url);
+      console.log("file url", file.ufsUrl);
 
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId };
