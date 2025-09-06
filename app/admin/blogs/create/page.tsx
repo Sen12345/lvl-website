f79edf0aea79475588d0d65807c127b531896082
Senator <senatorcox90@gmail.com>
2025-09-06 19:05:30 +0100

    Created a delete blog action

--- app/admin/blogs/create/page.tsx
+++ app/admin/blogs/create/page.tsx
@@ -1,19 +1,19 @@
-import { Metadata } from "next";
-import CreateBlogForm from "@/components/admin/create-blog-form";
-// import { requireAdmin } from "@/lib/auth-guard";
-export const metadata: Metadata = {
-  title: "Create Blog",
-};
+// import { Metadata } from "next";
+// import CreateBlogForm from "@/components/admin/create-blog-form";
+// // import { requireAdmin } from "@/lib/auth-guard";
+// export const metadata: Metadata = {
+//   title: "Create Blog",
+// };
 
-const CreateBlogPage = async () => {
-  // await requireAdmin();
-  return (
-    <>
-      <div className="flex justify-center">
-        <CreateBlogForm type="Create" />
-      </div>
-    </>
-  );
-};
+// const CreateBlogPage = async () => {
+//   // await requireAdmin();
+//   return (
+//     <>
+//       <div className="flex justify-center">
+//         <CreateBlogForm type="Create" />
+//       </div>
+//     </>
+//   );
+// };
 
-export default CreateBlogPage;
+// export default CreateBlogPage;
