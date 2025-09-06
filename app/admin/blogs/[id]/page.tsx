2e75ae95a81a3fba9f99647ed725af17b6081e15
Senator <senatorcox90@gmail.com>
2025-09-06 19:10:44 +0100

    Created a delete blog action

--- app/admin/blogs/[id]/page.tsx
+++ app/admin/blogs/[id]/page.tsx
@@ -1,26 +1,26 @@
-import UpdateBlogForm from "@/components/admin/update-blog-form";
-import { getBlogById } from "@/lib/actions/blog.actions";
-import { Metadata } from "next";
-import { notFound } from "next/navigation";
+// import UpdateBlogForm from "@/components/admin/update-blog-form";
+// import { getBlogById } from "@/lib/actions/blog.actions";
+// import { Metadata } from "next";
+// import { notFound } from "next/navigation";
 
-export const metadata: Metadata = {
-  title: "Update Blog",
-};
+// export const metadata: Metadata = {
+//   title: "Update Blog",
+// };
 
-const AdminBlogUpdate = async (props: { params: Promise<{ id: string }> }) => {
-  const { id } = await props.params;
+// const AdminBlogUpdate = async (props: { params: Promise<{ id: string }> }) => {
+//   const { id } = await props.params;
 
-  const blog = await getBlogById(id);
+//   const blog = await getBlogById(id);
 
-  if (!blog) return notFound();
-  // console.log(blog.id + "WWWWWWWWWWWWWWWWWWWWWWWWWWW");
-  return (
-    <div className="space-y-8 max-w-5xl mx-auto">
-      <h2 className="h2">Update Blog</h2>
-      {}
-      <UpdateBlogForm type="Update" blog={blog} blogId={blog.id} />
-    </div>
-  );
-};
+//   if (!blog) return notFound();
+//   // console.log(blog.id + "WWWWWWWWWWWWWWWWWWWWWWWWWWW");
+//   return (
+//     <div className="space-y-8 max-w-5xl mx-auto">
+//       <h2 className="h2">Update Blog</h2>
+//       {}
+//       <UpdateBlogForm type="Update" blog={blog} blogId={blog.id} />
+//     </div>
+//   );
+// };
 
-export default AdminBlogUpdate;
+// export default AdminBlogUpdate;
